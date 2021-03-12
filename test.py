cd8b16141acf65fdd5b83618620d4780772f8d10
Malaychamp <54617219+Malaychamp@users.noreply.github.com>
2021-03-12 16:40:03 +0530

    Update test.py
    added func3

--- test.py
+++ test.py
@@ -4,7 +4,11 @@ def func1():
 def func2():
 	print("Executing function2")
 
+def func3():
+	print("Executing function3")
 if __name__ == "__main__":
 	func1()
 	print("--------------------")
 	func2()
+	print("--------------------")
+	func3()
